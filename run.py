8afe2ba370a7396cc2f941cfd411c0598ef00ea1
Ryan Don <62915867+afinaldawn@users.noreply.github.com>
2022-11-28 18:52:36 -0800

    added run

--- run.py
+++ run.py
@@ -1,6 +1,18 @@
 from get_data import collect_data
 import model.build_model as bm
+import pandas as pd
 
-path = "data/clap_data.csv"
+raised = "data/raised_hand_data.csv"
+clap = "data/clap_data.csv"
 
-collect_data(25, path)
+# make sure you have the correct port in arduino in get_data.py, just for collecting clap data
+collect_data(25, clap)
+
+raised_df = pd.DataFrame(raised) 
+clap_df = pd.DataFrame(clap)
+df = bm.connect_df(raised_df, clap_df)
+df = bm.transform(df)
+model = bm.fit(df)
+
+# predict gestures on 10 gestures
+#collect_data(10, None, predicter=True, model=model)
