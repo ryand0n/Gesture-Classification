8df3ba5760fa87ce99aeefe76911dd4ff0c3bcf0
Ryan Don <62915867+afinaldawn@users.noreply.github.com>
2022-11-28 18:34:13 -0800

    added script to collect data

--- get_data.py
+++ get_data.py
@@ -6,6 +6,7 @@ import numpy as np
 import q as cute
 from itertools import count
 import csv
+from model.build_model import predict
 
 # can be used to automate finding right COM port
 def get_ports():
@@ -55,90 +56,101 @@ def get_data():
     idxs = [next(index) * 0.01 for _ in range(len(x))]
     print(x,y,z)
 
+def plot():
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
 
-fig = plt.figure()
-ax = fig.add_subplot(111)
+    i = 0
+    idxs = cute.Queue()
+    x_vals = cute.Queue()
+    y_vals = cute.Queue()
+    z_vals = cute.Queue()
 
-i = 0
-idxs = cute.Queue()
-x_vals = cute.Queue()
-y_vals = cute.Queue()
-z_vals = cute.Queue()
+    idxs.add(i)
+    x_vals.add(float(x))
+    y_vals.add(float(y))
+    z_vals.add(float(z))
 
-record = False
-index = 0
-all_data = []
-x_data = []
-y_data = []
-z_data = []
+    ax.plot(idxs.queue, x_vals.queue, color='b', label='x')
+    ax.plot(idxs.queue, y_vals.queue, color='r', label='y')
+    ax.plot(idxs.queue, z_vals.queue, color='g', label='z')
+    ax.legend()
+
+    fig.canvas.draw()
+
+    ax.set_xlim(left=max(0, i - 50), right=i + 50)
+    fig.show()
+    plt.pause(0.05)
+    i += 1
+
+    if idxs.size >= 50:
+        idxs.remove()
+    if x_vals.size >= 50:
+        x_vals.remove()
+    if y_vals.size >= 50:
+        y_vals.remove()
+    if z_vals.size >= 50:
+        z_vals.remove()
+
+def collect_data(i, path, predicter=False):
+    """
+    i: how many data points you want to collect
+    path: where you want to write your csv file to
+    """
+    record = False
+    index = 0
+    all_data = []
+    x_data = []
+    y_data = []
+    z_data = []
 
 
-while index < 50:
-    x,y,z = None,None,None
-    data = get_data()
+    while index < i:
+        x,y,z = None,None,None
+        data = get_data()
 
-    if data is not None:
-        print(data)
+        if data is not None:
+            print(data)
 
-    if data is not None and data[0] == 'data point recorded':
-        record = False
+        if data is not None and data[0] == 'data point recorded':
+            record = False
 
-    if data is not None and data[0] == '\x04Traceback (most recent call last):':
-        break
+        if data is not None and data[0] == '\x04Traceback (most recent call last):':
+            break
 
-    if data is not None and record and data[0] != 'data point recorded':
-        x,y,z = data
-        x,y,z = float(x), float(y), float(z)
-        x_data.append(x)
-        y_data.append(y)
-        z_data.append(z)
-    elif not record:
-        all_data.append({'index':index, 'x':x_data, 'y':y_data, 'z':z_data})
-        index += 1
-        x_data = []
-        y_data = []
-        z_data = []
-        record = True
-        print("data point recorded")
-        print(f"index: {index}")
-    else:
-        continue
+        if data is not None and record and data[0] != 'data point recorded':
+            x,y,z = data
+            x,y,z = float(x), float(y), float(z)
+            x_data.append(x)
+            y_data.append(y)
+            z_data.append(z)
+        elif not record:
+            all_data.append({'index':index, 'x':x_data, 'y':y_data, 'z':z_data})
+            
+            if predicter:
+                print(predict({'index':index, 'x':x_data, 'y':y_data, 'z':z_data}))
 
-    # idxs.add(i)
-    # x_vals.add(float(x))
-    # y_vals.add(float(y))
-    # z_vals.add(float(z))
+            index += 1
+            x_data = []
+            y_data = []
+            z_data = []
+            record = True
+            print("data point recorded")
+            print(f"index: {index}")
+        else:
+            continue
 
-    # ax.plot(idxs.queue, x_vals.queue, color='b', label='x')
-    # ax.plot(idxs.queue, y_vals.queue, color='r', label='y')
-    # ax.plot(idxs.queue, z_vals.queue, color='g', label='z')
-    # ax.legend()
+    csv_columns = ['index', 'x', 'y', 'z']
+    dict_data = all_data
+    csv_file = path
 
-    # fig.canvas.draw()
+    try:
+        with open(csv_file, 'w') as csvfile:
+            writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
+            writer.writeheader()
+            for data in dict_data:
+                writer.writerow(data)
+    except IOError:
+        print("I/O error")
 
-    # ax.set_xlim(left=max(0, i - 50), right=i + 50)
-    # fig.show()
-    # plt.pause(0.05)
-    # i += 1
-
-    # if idxs.size >= 50:
-    #     idxs.remove()
-    # if x_vals.size >= 50:
-    #     x_vals.remove()
-    # if y_vals.size >= 50:
-    #     y_vals.remove()
-    # if z_vals.size >= 50:
-    #     z_vals.remove()
-
-csv_columns = ['index', 'x', 'y', 'z']
-dict_data = all_data
-csv_file = "data/raised_hand_data.csv"
-
-try:
-    with open(csv_file, 'w') as csvfile:
-        writer = csv.DictWriter(csvfile, fieldnames=csv_columns)
-        writer.writeheader()
-        for data in dict_data:
-            writer.writerow(data)
-except IOError:
-    print("I/O error")
+    return dict_data
